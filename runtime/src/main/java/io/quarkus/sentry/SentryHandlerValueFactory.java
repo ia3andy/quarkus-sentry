bca8781897ed29e553e9d26e5970f76443d99946
ia3andy <adamevin@redhat.com>
2019-09-20 17:36:52 +0200

    Update extension following PR update

--- runtime/src/main/java/io/quarkus/sentry/SentryHandlerValueFactory.java
+++ runtime/src/main/java/io/quarkus/sentry/SentryHandlerValueFactory.java
@@ -5,17 +5,18 @@ import io.quarkus.runtime.annotations.Recorder;
 import io.sentry.Sentry;
 import io.sentry.jul.SentryHandler;
 
+import java.util.Optional;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 
 @Recorder
 public class SentryHandlerValueFactory {
 
-    public RuntimeValue<Handler> create(final SentryConfig config) {
+    public RuntimeValue<Optional<Handler>> create(final SentryConfig config) {
         System.setProperty("sentry.stacktrace.app.packages", config.packages);
         Sentry.init(config.dsn);
         SentryHandler handler = new SentryHandler();
         handler.setLevel(Level.WARNING);
-        return new RuntimeValue<>(handler);
+        return new RuntimeValue<>(Optional.of(handler));
     }
 }
